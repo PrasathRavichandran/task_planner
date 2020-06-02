5b2dd7ce369dcc6334c7529f0b9525bb97435cfe
Prasath Ravichandran <prasath.ravichandran@acodeidea.com>
2020-06-02 21:58:24 +0530

    task-planner page design is completed, new task, new list page design is completed, login, signup page design is completed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,14 +4,26 @@ import { Routes, RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { MainContainerComponent } from './main-container/main-container.component';
+import { NewListComponent } from './new-list/new-list.component';
+import { NewTaskComponent } from './new-task/new-task.component';
+import { LoginComponent } from './auth/login/login.component';
+import { SignupComponent } from './auth/signup/signup.component';
 
 const AppRoutes: Routes = [
-  { path: '', component: MainContainerComponent }
+  { path: '', component: MainContainerComponent },
+  { path: 'new-list', component: NewListComponent },
+  { path: 'new-task', component: NewTaskComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'signup', component: SignupComponent }
 ]
 @NgModule({
   declarations: [
     AppComponent,
-    MainContainerComponent
+    MainContainerComponent,
+    NewListComponent,
+    NewTaskComponent,
+    LoginComponent,
+    SignupComponent
   ],
   imports: [
     BrowserModule,
