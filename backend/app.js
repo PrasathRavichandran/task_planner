295d8fd1fa3ddcdd7403511cd30f939d53d9a6b6
Prasath Ravichandran <prasath.ravichandran@acodeidea.com>
2020-06-03 13:09:43 +0530

    Backend: CORS policy is added.

--- backend/app.js
+++ backend/app.js
@@ -15,6 +15,18 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 /**
+ * CORS policy
+ */
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
+
+/**
  * Application Routes
  */
 /** LISTS */
